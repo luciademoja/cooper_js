199af56ad9a9ba9ed4797a40a77d8528ceb5f165
Lucia De Mojà <luciademoja@gmail.com>
2016-08-10 12:13:49 +0200

    add Jasmine files and starts writing down the outline for the project

---
+++ src/Cooper.js
@@ -0,0 +1,13 @@
+function CooperTest(){
+};
+
+CooperTest.prototype.evaluate(obj) {
+  var gender = obj.gender;
+  var age = obj.age;
+  var distance = obj.distance;
+  switch()
+}
+
+function setTestResult (obj, value){
+  if (obj.)
+}
