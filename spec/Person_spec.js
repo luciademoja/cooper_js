d48e65cf3119639e48144bc21751f5c800062fb2
Lucia De Mojà <luciademoja@gmail.com>
2016-08-12 00:58:52 +0200

    test for accessing the score table when gender equals male

--- spec/Person_spec.js
+++ spec/Person_spec.js
@@ -1,17 +1,30 @@
 describe("Person", function() {
   var person;
+  beforeEach(function() {
+  person = new Person({gender: 'male', age: 14, distance: 2800});
+  });
 
-describe("Person Characteristics", function() {
-    beforeEach(function() {
-      person = new Person({gender: 'male', age: 14});
-    });
+  describe("Person Characteristics", function() {
+      it("Gender is defined and equals male", function() {
+        expect(person.gender).toBe('male');
+      });
 
-    it("should be a male", function() {
-      expect(person.gender).toEqual('male');
-    });
+      it("Age is defined and equals 14", function() {
+        expect(person.age).toBe(14);
+      });
 
-    it("should be 14 years old", function() {
-      expect(person.age).toEqual(14);
-    })
+      it("Distance is defined and set to 2800", function() {
+        expect(person.distance).toBe(2800);
+      });
+  });
 });
+
+describe("Cooper Test: Male", function() {
+  score = new CooperTest();
+  person = new Person({gender: 'male', age: 14, distance: 2800});
+
+  it("Access the male score table when gender equals male", function() {
+    score.testResults(person);
+    expect(person.message).toBe('Very Good');
+  });
 });
--- src/Person.js
+++ src/Person.js
@@ -1,4 +1,10 @@
 function Person(attr) {
   this.gender = attr.gender;
   this.age = attr.age;
+  this.distance = attr.distance;
+};
+
+Person.prototype.assessment = function() {
+  cooper = new CooperTest();
+  cooper.testResults(this);
 };
