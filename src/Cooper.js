6086d9f6280f6bfdc5516b4520af33d8b0b37799
Raoul DIFFOUO <diraulo@gmail.com>
2016-08-12 11:34:39 +0200

    convert range evaluation to js object

--- src/Cooper.js
+++ src/Cooper.js
@@ -14,25 +14,32 @@ CooperTest.prototype.testResults = function (obj) {
 };
 
 function maleResults (obj) {
-  var rangeOne = obj.age >= 13 && obj.age <= 14;
-  var rangeTwo = obj.age >= 15 && obj.age <= 16;
-  var rangeThree = obj.age >= 17 && obj.age <= 20;
-  var rangeFour = obj.age >= 21 && obj.age <= 29;
-  var rangeFive = obj.age >= 30 && obj.age <= 39;
-  var rangeSix = obj.age >= 40 && obj.age <= 49;
-  var rangeSeven = obj.age >= 50;
+  var ranges = getRanges(obj);
+
   var mDistanceOne = obj.distance >= 2700;
   // var mDistanceTwo = obj.distance >= 2400 && <= 2699;
   // var mDistanceThree = obj.distance >= 2200 && <= 2399;
   // var mDistanceFour = obj.distance >= 2100 && <= 2199;
   // var mDistanceFive = obj.distance <= 2100;
 
-  if(rangeOne) {
+  if(ranges.one) {
     if(obj.distance >= 2700) {
       obj.message = "Very Good";
     }
   }
-  if(rangeTwo && mDistanceOne) {
+  if(ranges.two && mDistanceOne) {
     obj.message = "Good";
   }
+}
+
+function getRanges(obj) {
+  return {
+    one: obj.age >= 13 && obj.age <= 14,
+    two: obj.age >= 15 && obj.age <= 16,
+    three: obj.age >= 17 && obj.age <= 20,
+    four: obj.age >= 21 && obj.age <= 29,
+    five: obj.age >= 30 && obj.age <= 39,
+    six: obj.age >= 40 && obj.age <= 49,
+    seven: obj.age >= 50
+  };
 }
--- src/Person.js
+++ src/Person.js
@@ -2,7 +2,7 @@ function Person(attr) {
   this.gender = attr.gender;
   this.age = attr.age;
   this.distance = attr.distance;
-};
+}
 
 Person.prototype.assessment = function() {
   cooper = new CooperTest();
